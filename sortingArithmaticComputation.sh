513fef3c145c552e03a2b7409bfcfc6dbf6db7e9
Aniruddha <aniruddha1712@gmail.com>
2022-03-12 18:55:30 +0530

    [Aniruddha]Added:feature to read value from Dict into array

--- sortingArithmaticComputation.sh
+++ sortingArithmaticComputation.sh
@@ -14,7 +14,13 @@ comp4=$(($a % $b + $c))
 
 declare -A computationDict
 
-computationDict[a+b*c]=[$comp1]
-computationDict[a*b+c]=[$comp2]
-computationDict[c+a/b]=[$comp3]
-computationDict[a%b+c]=[$comp4]
+computationDict[a+b*c]=$comp1
+computationDict[a*b+c]=$comp2
+computationDict[c+a/b]=$comp3
+computationDict[a%b+c]=$comp4
+echo "${computationDict[@]}"
+
+declare -a computationArr
+
+computationArr=( ${computationDict[a+b*c]} ${computationDict[a*b+c]} ${computationDict[c+a/b]} ${computationDict[a%b+c]} )
+echo "${computationArr[@]}"
