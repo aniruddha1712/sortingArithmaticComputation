584b2cd4d02d5dcdd92664d9f2b860c3c23fa552
Aniruddha <aniruddha1712@gmail.com>
2022-03-12 18:08:35 +0530

    [Aniruddha]Added: Dictionary to store result

--- sortingArithmaticComputation.sh
+++ sortingArithmaticComputation.sh
@@ -11,3 +11,10 @@ comp2=$(($a * $b + $c))
 comp3=$(($c + $a / $b))
 
 comp4=$(($a % $b + $c))
+
+declare -A computationDict
+
+computationDict[a+b*c]=[$comp1]
+computationDict[a*b+c]=[$comp2]
+computationDict[c+a/b]=[$comp3]
+computationDict[a%b+c]=[$comp4]
